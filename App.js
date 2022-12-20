7f5bcc8a624cc13de51638d07fe7fc165fc3036f
ryan <rayanuki@outlook.com>
2022-12-20 20:34:48 +0800

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,92 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useRef, useState } from "react";
+import { StyleSheet, Text, View, Image, ScrollView, SafeAreaView, StatusBar, DrawerLayoutAndroid, Button } from 'react-native';
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+  const drawer = useRef(null);
+  const [drawerPosition, setDrawerPosition] = useState("left");
+
+  const navigationView = () => (
+    <View style={[styles.container, styles.navigationContainer]}>
+      <Text style={styles.paragraph}>I'm in the Drawer!</Text>
+      <Button
+        title="Close drawer"
+        onPress={() => drawer.current.closeDrawer()}
+      />
     </View>
+  );
+
+  return (
+      <SafeAreaView style={styles.container}>
+        <DrawerLayoutAndroid
+        ref={drawer}
+        drawerWidth={300}
+        drawerPosition={drawerPosition}
+        renderNavigationView={navigationView}
+      >
+        <View style={[styles.container, {flexDirection:"column"}]}>
+          <StatusBar />
+          <View style={{
+            width: '100%', height: 80, backgroundColor: 'powderblue'
+          }}>
+            <View style={[styles.container, {flexDirection:"row"}]}>
+                <View style={{ flex: 1, backgroundColor: "green" }}></View>
+                <View style={{ flex: 5, backgroundColor: "blue" }}></View>
+            </View>
+          </View>
+          <View style={{ flex: 6, backgroundColor: "#fff" }}>
+            <ScrollView>
+            <Button
+          title="Open drawer"
+          onPress={() => drawer.current.openDrawer()}
+        />
+              <Text style={{ fontSize: 96 }}>Scroll me plz</Text>
+              <Image source={logo} />
+              <Image source={logo} />
+              <Image source={logo} />
+              <Image source={logo} />
+              <Image source={logo} />
+              <Text style={{ fontSize: 96 }}>If you like</Text>
+              <Image source={logo} />
+              <Image source={logo} />
+              <Image source={logo} />
+              <Image source={logo} />
+              <Image source={logo} />
+              <Text style={{ fontSize: 96 }}>Scrolling down</Text>
+              <Image source={logo} />
+              <Image source={logo} />
+              <Image source={logo} />
+              <Image source={logo} />
+              <Image source={logo} />
+              <Text style={{ fontSize: 96 }}>What's the best</Text>
+              <Image source={logo} />
+              <Image source={logo} />
+              <Image source={logo} />
+              <Image source={logo} />
+              <Image source={logo} />
+              <Text style={{ fontSize: 96 }}>Framework around?</Text>
+              <Image source={logo} />
+              <Image source={logo} />
+              <Image source={logo} />
+              <Image source={logo} />
+              <Image source={logo} />
+              <Text style={{ fontSize: 80 }}>React Native</Text>
+            </ScrollView>
+          </View>
+        </View>
+      </DrawerLayoutAndroid>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 0,
   },
-});
+}),
+logo = {
+  uri: 'https://reactnative.dev/img/tiny_logo.png',
+  width: 64,
+  height: 64
+};
